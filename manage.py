703c40a7c55746dd14b9790432accab5bbf33977
angels101 <angelscodex101@gmail.com>
2020-10-06 08:26:09 +0300

    modifications

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager,Server
 
 # Creating app instance can you hear me??
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
